a558d2d3a24e9c8a7056b04475c6876ee7506955
Alexander <36491066+danube88@users.noreply.github.com>
2021-08-17 23:52:20 +0300

    breeze install

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-		'prefix' => LaravelLocalization::setLocale(),
-		'middleware' => [
-			'localeSessionRedirect',
-			'localizationRedirect',
-			'localeViewPath'
-		]
-	], function() {
-        Route::get('/', function () {
-            return view('welcome');
-        });
-	});
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
